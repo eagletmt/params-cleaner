178de1c1c2a8f7fdc9a09417e0f9a17222d8edea
Kohei Suzuki <eagletmt@gmail.com>
2014-03-01 20:25:14 +0900

    amazon: Ignore ref_ param

--- test/test-amazon.js
+++ test/test-amazon.js
@@ -11,6 +11,11 @@ exports['test remove unnecessary search params'] = function(assert) {
   assert.equal(amazon.clean(uri), 'http://www.amazon.co.jp/s?rh=n%3A2127209051%2Cn%3A!2127210051%2Cn%3A2151981051%2Cp_36%3A5000000-5999900');
 };
 
+exports['test remove unnecessary params in feature.html'] = function(assert) {
+  let uri = util.makeURI('http://www.amazon.co.jp/gp/feature.html?ref_=tsm_1_tw_s_ajpk_n1r5tr&ie=UTF8&docId=3077680066');
+  assert.equal(amazon.clean(uri), 'http://www.amazon.co.jp/gp/feature.html?docId=3077680066');
+};
+
 exports["test don't modify other site's params"] = function(assert) {
   let uri = util.makeURI('http://example.com/gp/product/B009WQ63AE/ref=s9_simh_gw_p351_d0_i7?pf_rd_m=AN1VRQENFRJN5&pf_rd_s=center-5&pf_rd_r=0KTB3ERFSFSFGFDXSGH3&pf_rd_t=101&pf_rd_p=155416549&pf_rd_i=489986');
   assert.equal(amazon.clean(uri), null);
